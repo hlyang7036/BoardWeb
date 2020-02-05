cd6da0311c80cd25e58430e242095d4760ef3dda
Hlyang <Hlyang7036@gmail.com>
2020-02-05 21:11:19 +0900

    security관련 admin페이지, system페이지 추가

--- src/main/java/com/example/Board/security/SecurityConfig.java
+++ src/main/java/com/example/Board/security/SecurityConfig.java
@@ -1,5 +1,27 @@
 package com.example.board.security;
 
-public class SecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private SecurityUserDetailsService userDetailsService;
+
+	@Override
+	protected void configure(HttpSecurity security) throws Exception {
+		security.userDetailsService(userDetailsService);
+		
+		security.authorizeRequests().antMatchers("/", "/system/**").permitAll();
+		security.authorizeRequests().antMatchers("/board/**").authenticated();
+		security.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN");
+		
+		security.csrf().disable();
+		security.formLogin().loginPage("/system/login").defaultSuccessUrl("/board/getBoardList", true);
+		security.exceptionHandling().accessDeniedPage("/system/accessDenied");
+		security.logout().logoutUrl("/system/logout").invalidateHttpSession(true).logoutSuccessUrl("/");
+	}
 }
